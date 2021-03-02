a0f2bdbc508bb48ff7c16ba122e184e07cfe8626
Mai Thành Thắng <52226980+Thang1102@users.noreply.github.com>
2021-03-02 10:14:31 +0700

    Update Script_install_ssl_and_Valid_Trusted_Certificate.sh

--- Script_install_ssl_and_Valid_Trusted_Certificate.sh
+++ Script_install_ssl_and_Valid_Trusted_Certificate.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#
-# Step 1 — Installing the Certbot Let’s Encrypt Client
-# To add the CentOS 8 EPEL repository, run the following command:
-yum -y install epel-release
-# Now that you have access to the repository, install all of the required packages:
-yum -y install certbot python3-certbot-apache mod_ssl
-# Step 2 - Create virtual host
 cd /etc/httpd/conf.d/
 touch thangblog274.tk.conf
 echo "
@@ -17,4 +10,3 @@ echo "
     CustomLog /var/log/httpdrequests.log combined
 </VirtualHost>
 " >> thangblog274.tk.conf
-echo "Manual install Certificate use file Setup_LetsEncrypt"
