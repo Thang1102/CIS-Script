9d0aea1d57fa35e599092e36708617b00deecc2a
Mai Thành Thắng <52226980+Thang1102@users.noreply.github.com>
2021-05-25 21:50:11 +0700

    Update Script_disable_module_proxy.sh

--- Script_disable_module_proxy.sh
+++ Script_disable_module_proxy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /etc/httpd/conf.modules.d/
 mv 00-proxy.conf 00-proxy.conf.bak
-# mv 10-proxy_h2.conf 10-proxy_h2.conf.bak
 cd /etc/httpd/conf.d/
 mv autoindex.conf autoindex.conf.bak
 cd root
